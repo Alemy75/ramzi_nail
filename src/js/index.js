ffd9094d76ce9d78e236f47e358b0a4438554120
Alemax75 <alemax75962@gmail.com>
2023-04-09 08:54:59 +0300

    fix: добавли навеедние на offer

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,12 @@
+// Popup
 let popup = document.querySelector('.popup-nav');
 let openPopupButton = document.getElementById('popup-nav_open');
 let closePopupButton = document.getElementById('popup-nav_close'); 
 
+// Offers
+let offer = document.getElementById('offers-img');
+
+// Popup events
 openPopupButton.addEventListener('click', () => {
 	popup.classList.add('popup-nav_active')
 })
