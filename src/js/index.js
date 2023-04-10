2183a18afebed0441c3163b84327183c3a7870b4
Alemax75 <alemax75962@gmail.com>
2023-04-10 14:31:24 +0300

    Добавил ramzil_school стр

--- src/js/index.js
+++ src/js/index.js
@@ -8,42 +8,40 @@ let offer = document.getElementById('offers-img');
 
 // Popup events
 openPopupButton.addEventListener('click', () => {
-    popup.classList.add('popup-nav_active')
+  popup.classList.add('popup-nav_active')
 })
 
 closePopupButton.addEventListener('click', () => {
-    popup.classList.remove('popup-nav_active')
+  popup.classList.remove('popup-nav_active')
 })
 
 // Smooth links
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
 for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener('click', function (e) {
-        e.preventDefault();
-        const id = smoothLink.getAttribute('href');
+  smoothLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute('href');
 
-        document.querySelector(id).scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-        popup.classList.remove('popup-nav_active')
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
     });
+    popup.classList.remove('popup-nav_active')
+  });
 };
 
 // Scroll anim
-
 function onEntry(entry) {
-    entry.forEach(change => {
-      if (change.isIntersecting) {
-        change.target.classList.add('element-show');
-      }
-    });
-  }
-  let options = { threshold: [0] };
-  let observer = new IntersectionObserver(onEntry, options);
-  let elements = document.querySelectorAll('.element-animation');
-  for (let elm of elements) {
-    observer.observe(elm);
-  }
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
 
-  
